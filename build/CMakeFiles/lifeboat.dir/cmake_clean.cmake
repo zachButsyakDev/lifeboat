b5a82eb49288bf42211f3a7cb68ee1ecde369d7a
tjoaks <tjoaks22@gmail.com>
2026-01-17 12:09:31 -0500

    setup cmake

---
+++ build/CMakeFiles/lifeboat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lifeboat.dir/src/main.c.o"
+  "CMakeFiles/lifeboat.dir/src/main.c.o.d"
+  "lifeboat"
+  "lifeboat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lifeboat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
