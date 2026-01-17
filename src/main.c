eabf96dc8ccb03cbf1de2b1c9ee52b557acf4cd3
tjoaks <tjoaks22@gmail.com>
2026-01-17 12:30:37 -0500

    setup sdl2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
 #include <stdio.h>
+// main function just handles sdl2, windows, runtime loop
 
 int main() {
-  printf("Hello World");
+
+  printf("Hello World\n");
   return 0;
 }
