83071ca51614929c2d551129ac1a294bc7b1355b
tjoaks <tjoaks22@gmail.com>
2026-01-17 13:47:46 -0500

    upgrade to C23, handle some inputs

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_video.h>
 #include <SDL2_image/SDL_image.h>
 #include <stdbool.h>
@@ -29,8 +30,23 @@ int main(int argc, char *argv[]) {
     SDL_Event event;
 
     while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
+      switch (event.type) {
+      case SDL_MOUSEMOTION:
+        printf("Mouse is moving.\n");
+        break;
+      case SDL_KEYDOWN:
+        const char *keyName = SDL_GetKeyName(event.key.keysym.sym);
+        if (event.key.keysym.sym == SDLK_x || event.key.keysym.sym == SDLK_z ||
+            event.key.keysym.sym == SDLK_UP ||
+            event.key.keysym.sym == SDLK_DOWN ||
+            event.key.keysym.sym == SDLK_LEFT ||
+            event.key.keysym.sym == SDLK_RIGHT) {
+          printf("%s key was pressed.\n", keyName);
+        }
+        break;
+      case SDL_QUIT:
         isRunning = false;
+        break;
       }
     }
   }
